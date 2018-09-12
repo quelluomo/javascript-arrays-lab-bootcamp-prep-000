683b29e4beaf1e49ee4a4a6c5004d698b46ff2ff
Matteo Muscas <thatman.j@gmail.com>
2018-09-12 13:39:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function prependKitten(name){
   return moreKittens;
 }
 function removeLastKitten(){
-    kittens.slice()
-    return kittens;
+    var lessKittens = kittens.slice(2)
+    return lessKittens
 }
